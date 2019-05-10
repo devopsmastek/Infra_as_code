02b5dad565839efba245a4c362577e9e48c7b6c5
Dhawan Shringi <dhawan12582@mastek.com>
2019-05-10 15:46:40 +0530

    Code refactoring

--- instances.tf
+++ instances.tf
@@ -4,24 +4,20 @@ resource "aws_key_pair" "terraformkeypair" {
 }
 
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+  name = "allow_tls"
   description = "Allow TLS inbound traffic"
-
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-
-
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -31,24 +27,24 @@ resource "aws_instance" "ansible" {
   key_name = "${aws_key_pair.terraformkeypair.key_name}"
   security_groups = ["allow_tls"]
 
-#  provisioner "file" {
-#    source = "playbook.yml"
-#    destination = "/etc/ansible/playbook/playbook.yml"
-#     }
-
-
-
+  /*
+  provisioner "file" {
+    source = "playbook.yml"
+    destination = "/etc/ansible/playbook/playbook.yml"
+  }
+  */
   provisioner "remote-exec" {
-
     inline = [
-#      "sleep 90",
- #     "sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y",
- #     "sudo yum install ansible -y",
- #     "ansible --version",
-#         "git clone https://devopsmastek:Mastek%401234@github.com/devopsmastek/assemblyline.git",
-#         "sudo ansible-playbook  /home/ubuntu/assemblyline/ansible/playbook.yml"
+      /*
+      "sleep 90",
+      "sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y",
+      "sudo yum install ansible -y",
+      "ansible --version",
+      "git clone https://devopsmastek:Mastek%401234@github.com/devopsmastek/assemblyline.git",
+      "sudo ansible-playbook  /home/ubuntu/assemblyline/ansible/playbook.yml"
+      */
 
-"sudo apt-get update",
+      "sudo apt-get update",
       "sleep 10",
       "sudo apt-get install software-properties-common",
       "sleep 10",
@@ -59,18 +55,16 @@ resource "aws_instance" "ansible" {
       "ansible --version",
       "git clone -b dev  https://devopsmastek:Mastek%401234@github.com/devopsmastek/assemblyline.git",
       "sleep 30",
-      "sudo -H  ansible-playbook  /home/ubuntu/assemblyline/ansible/playbook.yml"
-
-          ]}
+      "sudo -H ansible-playbook /home/ubuntu/assemblyline/ansible/playbook.yml"
+    ]
+  }
 
   connection {
-      type = "ssh",
-      user = "ubuntu"
-      private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-      timeout = "3m"
-      agent = false
-      }
-
-
+    type = "ssh",
+    user = "ubuntu"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+    timeout = "3m"
+    agent = false
+  }
 
 }
